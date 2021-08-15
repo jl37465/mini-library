21b88e1929560c62564fdd230370d71e3587e780
Joel Lee <leejoel010@gmail.com>
2021-08-15 19:51:22 +0100

    Books in library array now display as individual cards when createNewCard is called

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.readState = null;
     this.position = null;
     this.info = function () {
-        let readState;
 
         if (read.toLowerCase() === "yes" || read.toLowerCase() === "y") {
             readState = "read";
@@ -21,11 +21,39 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
-    book.position = myLibrary.length;
+    book.position = myLibrary.indexOf(book);
     return `"${book.title}" added to library!`;
 }
 
-function updateTable(data) {
+function createNewCard(book) {
+    let cardContainer = document.getElementById("card-container");
+    let newCard = document.createElement("div");
+    let titleText = document.createElement("p");
+    let authorText = document.createElement("p");
+    let pagesText = document.createElement("p");
+    let readYetText = document.createElement("p");
+
+    titleText.className = "title-text";
+    titleText.textContent = book.title;
+    authorText.className = "author-text";
+    authorText.textContent = book.author;
+    pagesText.className = "pages-text";
+    pagesText.textContent = book.pages;
+    readYetText.className = "read-yet-text";
+    readYetText.textContent = book.read;
+    newCard.className = "book-card";
+
+    newCard.appendChild(titleText);
+    newCard.appendChild(authorText);
+    newCard.appendChild(pagesText);
+    newCard.appendChild(readYetText);
+    cardContainer.appendChild(newCard);
+
+    addBookToLibrary(book);
+    console.log(`${book.title} card added!`)
+}
+
+/* function updateTable(data) {
     let htmlBody = document.getElementById("the-body");
     let table = document.createElement("table");
 
@@ -43,7 +71,7 @@ function updateTable(data) {
     readYetCell.textContent = "READ?";
 
 
-    /* for (let i = 1; i < data.length + 1; i++) {
+    for (let i = 1; i < data.length + 1; i++) {
         let newRow = table.insertRow(i);
         let title = newRow.insertCell(0);
         let author = newRow.insertCell(1);
@@ -52,7 +80,7 @@ function updateTable(data) {
 
         title.textContent = data[i].title;
 
-    } */
+    }
     
     data.forEach(function(item) {
         let newRow = table.insertRow();
@@ -68,23 +96,33 @@ function updateTable(data) {
 
     htmlBody.textContent = "";
     htmlBody.appendChild(table);
-}
+} */
 
-let example1 = new Book("Hello", "Mr. Brown", 164, "Yes");
-let example2 = new Book("Wus gud", "Ms. Keaton", 351, "Yes");
-let example3 = new Book("EW EW EW", "Some Kid", 6, "No");
+let example1 = new Book("Hello", "Mr. Brown", 164, true);
+let example2 = new Book("Wus gud", "Ms. Keaton", 351, false);
+let example3 = new Book("EW EW EW", "Some Kid", 6, true);
 
-console.log(addBookToLibrary(example1));
-console.log(addBookToLibrary(example2));
-console.log(addBookToLibrary(example3));
+createNewCard(example1);
+createNewCard(example2);
+createNewCard(example3);
+createNewCard(example1);
+createNewCard(example2);
+createNewCard(example3);
+createNewCard(example1);
+createNewCard(example2);
+createNewCard(example3);
+createNewCard(example1);
+createNewCard(example2);
+createNewCard(example3);
 
-updateTable(myLibrary);
+
+/* updateTable(myLibrary); */
 
 /*
 
->>>>TO DO:<<<<
+>>>>TO DO:<<<<read.toLowerCase() === "no" || read.toLowerCase() === "n"
 - Remove table stuff and insert a new card (div) for each individual book
-with all the appropriate information.
+with all the appropriate information. DONE >>>>><<<<<<
 - Ensure that the formatting (dimensions, height, width, etc.) are all
 consistent for each card.
 - Add a button that creates a form to add a new book:
